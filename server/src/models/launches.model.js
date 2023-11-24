e31ec779c68b6cad230546ad4c829bb8ad337793
Frajdi <f.malaj@teamsystem.com>
2023-11-24 20:20:47 +0100

    got launches from spacex api

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,29 +1,114 @@
 const launches = require("./launches.mongo");
 const planets = require("./planets.mongo");
+const axios = require("axios");
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launchExists = async (launchId) => {
-  return await launches.findOne({ flightNumber: launchId });
+const LAUNCHES_SPACEX_API = "https://api.spacexdata.com/v4/launches/query";
+
+const populateLaunches = async () => {
+  console.log("Loading Launches...");
+  const response = await axios.post(LAUNCHES_SPACEX_API, {
+    query: {},
+    options: {
+      pagination: false,
+      populate: [
+        {
+          path: "rocket",
+          select: {
+            name: 1,
+          },
+        },
+        {
+          path: "payloads",
+          select: {
+            customers: 1,
+          },
+        },
+      ],
+    },
+  });
+
+  if(response.status !== 200){
+    throw new Error('Failed to load launches...')
+  }
+
+  const launchDocs = response.data.docs;
+
+  for (const launchDoc of launchDocs) {
+    const customers = launchDoc.payloads.flatMap((payload) => {
+      return payload["customers"];
+    });
+
+    const launch = {
+      flightNumber: launchDoc["flight_number"],
+      launchDate: launchDoc["date_local"],
+      mission: launchDoc["name"],
+      rocket: launchDoc["rocket"]["name"],
+      success: launchDoc["success"],
+      upcoming: launchDoc["upcoming"],
+      customers,
+    };
+
+    console.log('-----------',launch);
+
+    await saveLaunch(launch);
+
+    // console.log(`${launch.flightNumber} ${launch.mission}`);
+  }
 };
 
-const getLatestFlightNumber = async() => {
-  const latestLaunch = await launches
-    .findOne()
-    .sort('-flightNumber');
+const loadLaunches = async () => {
+  const loadedLaunch = await findLaunch({
+    launchDate: "2006-03-24T22:30:00.000+00:00",
+    mission: "FalconSat",
+    rocket: "Falcon 1",
+  });
 
-  if(!latestLaunch){
+  if (loadedLaunch) {
+    console.log("Launches already loaded.");
+    return;
+  } else {
+    await populateLaunches();
+  }
+};
+
+const findLaunch = async (filter) => {
+  return await launches.findOne(filter);
+}; 
+
+const launchExists = async (launchId) => {
+  return await findLaunch({ flightNumber: launchId });
+};
+
+const getLatestFlightNumber = async () => {
+  const latestLaunch = await launches.findOne().sort("-flightNumber");
+
+  if (!latestLaunch) {
     return DEFAULT_FLIGHT_NUMBER;
   }
 
   return latestLaunch.flightNumber + 1;
-}
+};
 
 const getAllLaunches = async () => {
   return await launches.find({}, { _id: 0, __v: 0 });
 };
 
+const saveLaunch = async (launch) => {
+  return await launches.findOneAndUpdate(
+    {
+      flightNumber: launch.flightNumber,
+    },
+    launch,
+    {
+      upsert: true,
+    }
+  );
+};
+
 const addNewLaunch = async (launch) => {
+
   const planet = await planets.findOne({ keplerName: launch.target });
 
   if (!planet){
@@ -37,17 +122,9 @@ const addNewLaunch = async (launch) => {
     customers: ["Frajdi", "Beni"],
     success: true,
     upcoming: true,
-  })
+  });
 
-  return await launches.findOneAndUpdate(
-      {
-        flightNumber: newFlightNumber,
-      },
-      newLaunch,
-      {
-        upsert: true,
-      }
-    );
+  await saveLaunch(newLaunch);
 };
 
 const deleteLaunch = async (id) => {
@@ -60,6 +137,7 @@ const deleteLaunch = async (id) => {
 };
 
 module.exports = {
+  loadLaunches,
   launchExists,
   getAllLaunches,
   addNewLaunch,
--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const app = require('./app');
 const {mongoConnect} = require('./services/mongo')
 const {loadPlanetsData} = require('./models/planets.model');
+const {loadLaunches} = require('./models/launches.model');
 
 const PORT = process.env.PORT || 4000;
 
@@ -9,7 +10,9 @@ const server = http.createServer(app);
 
 const startServer = (async() => {
  
-    await mongoConnect()
+    await mongoConnect();
+
+    await loadLaunches();
 
     await loadPlanetsData();
     
