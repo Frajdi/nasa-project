8f716523e94d3ab3a9e4e8dd01d10e675ef3220c
Frajdi <f.malaj@teamsystem.com>
2023-12-09 17:05:26 +0100

    fixed tests 2for local & pipeline

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -3,12 +3,14 @@ const app = require('../../app')
 const {
     mongoConnect,
     mongoDisconnect
-} = require('../../services/mongo')
+} = require('../../services/mongo');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('Test our launches API', () => {
 
     beforeAll(async() => {
         await mongoConnect();
+        await loadPlanetsData();
     });
 
     afterAll(async() => {
